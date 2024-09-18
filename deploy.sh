5bf8bbac434d12d19a38048204fc11656dcd36a8
Tsadok Levi <tsadok.levi@CB--Tsadok.Levi>
2024-09-18 12:31:15 +0530

    feat(deploy): adding deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
+set -e
 
-# TODO your deploy script implementation...
+echo ""
+echo "Creating a Python virtual environment..."
+echo "--------------------------------"
+python3 -m venv venv
+
+echo ""
+echo "Activating the Python virtual environment..."
+echo "--------------------------------"
+source venv/bin/activate
+
+echo ""
+echo "Install Python dependencies..."
+echo "--------------------------------"
+pip install -r requirements.txt
+
+echo ""
+echo "Starting the Python application..."
+echo "--------------------------------"
+python app.py
